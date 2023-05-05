9fc17281a523e4d21c04b3e9ea5795c47397f434
Prathik <prathikk017@gmail.com>
2023-05-05 21:56:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Main from "./components/Main"
+import Navbar from "./components/Navbar"
+import Offers from "./components/Offers"
+import Plan from "./components/Plan"
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Navbar/>
+        <Main/>
+        <Offers/>
+        <Plan/>
+        </div>
+    </>
+  )
+}
+
+export default App
