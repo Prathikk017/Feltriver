243db41483af34ecbcb6ed96c0b735eba5867516
Prathik <prathikk017@gmail.com>
2023-05-05 22:01:46 +0530

    added navbar and offers page

---
+++ src/components/Offers.jsx
@@ -0,0 +1,14 @@
+
+
+const Offers = () => {
+  return (
+    <div className="max-w-[900px] m-auto px-4 py-12 flex flex-wrap justify-between">
+        <p className="text-lg font-bold text-gray-700 cursor-pointer">Booking Incentives</p>
+        <p className="text-lg font-bold text-gray-700 cursor-pointer">Competitive Rates</p>
+        <p className="text-lg font-bold text-gray-700 cursor-pointer">Book with Flexibility</p>
+        <p className="text-lg font-bold text-gray-700 cursor-pointer">Trip Insurance</p>
+    </div>
+  )
+}
+
+export default Offers
